c9982ff4be51f1c1b524837620a87cb8030ea238
Gloriah-B <Gloriah-B@example.com>
2024-08-12 11:53:50 -0700

    12-log_stats.py

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python3
 '''Task 12's module.
+This module provides functionality to print statistics about Nginx request logs 
+stored in a MongoDB collection.
 '''
 from pymongo import MongoClient
 
 
 def print_nginx_request_logs(nginx_collection):
-    '''Prints stats about Nginx request logs.'''
+    '''Prints stats about Nginx request logs.
+
+    Args:
+        nginx_collection (pymongo.collection.Collection): The MongoDB collection 
+        containing the Nginx logs.
+
+    Prints:
+        The number of logs and the count of each HTTP method found in the logs.
+    '''
     print('{} logs'.format(nginx_collection.count_documents({})))
     print('Methods:')
     methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
