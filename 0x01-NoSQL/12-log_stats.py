c8226f62ca1c19442f17d739c63dd62319b5d2ad
Gloriah-B <Gloriah-B@example.com>
2024-08-12 12:03:28 -0700

    12-log_stats.py

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -8,18 +8,18 @@ def print_nginx_request_logs(nginx_collection):
     '''Prints statistics about Nginx request logs.
 
     Args:
-        nginx_collection (pymongo.collection.Collection): The MongoDB collection 
+        nginx_collection (pymongo.collection.Collection): MongoDB collection
         containing the Nginx logs.
     '''
     total_logs = nginx_collection.count_documents({})
     print(f'{total_logs} logs')
     print('Methods:')
-    
+
     methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
     for method in methods:
         method_count = nginx_collection.count_documents({'method': method})
         print(f'\tmethod {method}: {method_count}')
-    
+
     status_check_count = nginx_collection.count_documents({
         'method': 'GET', 'path': '/status'
     })
@@ -27,7 +27,7 @@ def print_nginx_request_logs(nginx_collection):
 
 
 def run():
-    '''Connects to MongoDB and prints stats about Nginx logs stored in the collection.'''
+    '''Connects MongoDB n prints stats abt Nginx logs stored in collection'''
     client = MongoClient('mongodb://127.0.0.1:27017')
     nginx_collection = client.logs.nginx
     print_nginx_request_logs(nginx_collection)
